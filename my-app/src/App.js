a06343a5625fcf2294721fe3549d10608fcaf49c
Giozee <zetorulez@gmail.com>
2021-12-13 11:12:58 +0100

    grass
    added grass

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,50 +1,23 @@
-import React, { useState, useRef } from 'react';
-import { Canvas, useFrame } from 'react-three-fiber';
+import React, { Suspense  } from 'react';
+import { Canvas } from 'react-three-fiber';
 import { Sky } from 'drei';
 import { Stars } from 'drei';
-import { Physics } from 'use-cannon';
-import { Html } from 'drei';
-import { Ground } from './components/Ground';
-import Cubes from './components/Cubes';
-import { Player } from './components/Player';
-import { Box } from 'drei';
-import { BooleanKeyframeTrack } from 'three';
 import { OrbitControls } from 'drei';
-
+import Grass from "./Grass"
 
 function App() {
   return (
-    <Canvas shadowMap sRGB>
- {/* 
-<Html
+    <Canvas>
 
-  position={[-14.8, 9.0, 0.5]}
-  style={{
-    width: 1920,
-    height: 1080,
-    background: 'white',
-          transition: 'all 0.2s',
-
-        }}
->
-
-      <img src="https://i.imgur.com/Hzjjtc2.jpg" width="1920" height="1080"></img>
-
-</Html>
-*/}
-    <Stars radius={80} depth={50} count={5000} factor={3} saturation={0} fade />
-      <Sky distance={450000} sunPosition={[0, 0, 0]} azimuth={0.0} />
+    <Stars radius={80} depth={50} count={5000} factor={5} saturation={0} fade />
+      <Sky distance={1000} sunPosition={[0, 10, 0]} azimuth={15.0} inclination={12.6} />
 
 
-      <ambientLight intensity={0.0} />
-      <pointLight castShadow intensity={0.1} position={[100, 100, 100]} />
-
-      <Physics gravity={[0, 0, 0]}>
+      <Suspense fallback={null}>
+        <Grass />
+      </Suspense>
         
-      <OrbitControls autoRotate autoRotateSpeed='0.15' minDistance = '250' maxDistance = '250' enabled = {[false]} />
-<mesh />
-        <Cubes />
-      </Physics>
+      <OrbitControls  autoRotate autoRotateSpeed='0.25' minDistance = '50' maxDistance = '50' enabled = {[false]} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} />
     </Canvas>
     
     
